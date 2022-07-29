aa997c976cf57804c0b384a77ce926536f330596
safym <osafym@gmail.com>
2022-07-30 01:12:50 +0300

    создана корзина товаров

--- src/components/content/Catalog.js
+++ src/components/content/Catalog.js
@@ -1,11 +1,32 @@
 //import modules
 import React, { useState, useEffect } from "react";
 
+// import icons
+import {FaShoppingCart} from "react-icons/fa";
+
 const Catalog = () => {
-  return <PhoneItems />;
+  let [cart, setCart] = useState([]);
+  let [cartOpen, setCartOpen] = useState(false);
+
+  const addToCart = (item) => {
+    setCart([...cart, item.data])
+  };
+
+  const openCart = () => {
+    setCartOpen(true)
+  };
+
+
+  return (
+    <div className="Content">
+      <PhoneItems callback={addToCart} />
+      <Cart items={cart} cartOpen={cartOpen}/>
+      <ButtonCart className="OpenCart" cart={cart} callback={openCart}/>
+    </div>
+  );
 };
 
-const PhoneItems = () => {
+const PhoneItems = (props) => {
   const [error, setError] = useState(null);
   const [items, setItems] = useState([]);
 
@@ -15,7 +36,6 @@ const PhoneItems = () => {
       .then(
         (result) => {
           setItems(result.message.result);
-          console.log(result.message.result);
         },
         (error) => {
           setError(error);
@@ -27,32 +47,61 @@ const PhoneItems = () => {
     return <div>Ошибка: {error.message}</div>;
   } else {
     return (
-      <div className="Content">
-        <div className="catalog">
-          {items.map((data) => (
-            <div className="phoneItem" key={"item" + data.ID}>
-              <h2 className="itemYear" key={"itemYear" + data.ID}>
-                {" "}
-                {data.Announced}{" "}
-              </h2>
-              <img
-                className="itemImage"
-                src={require("../../img/catalog/" + data.Image)}
-              />
-              <h1 className="itemName" key={"itemName" + data.ID}>
-                {" "}
-                {data.Name}{" "}
-              </h1>
-              <h2 className="itemPrice" key={"itemPrice" + data.ID}>
-                {" "}
-                {data.Price}${" "}
-              </h2>
-            </div>
-          ))}
-        </div>
+      <div className="catalog">
+        {items.map((data) => (
+          <div className="phoneItem" key={"item" + data.ID}>
+            <h2 className="itemYear" key={"itemYear" + data.ID}>
+              {data.Announced}
+            </h2>
+            <img
+              className="itemImage"
+              src={require("../../img/catalog/" + data.Image)}
+            />
+            <h1 className="itemName" key={"itemName" + data.ID}>
+              {data.Name}
+            </h1>
+            <h2 className="itemPrice" key={"itemPrice" + data.ID}>
+              {data.Price + " "}$
+            </h2>
+            <button className="myButton" type="submit" onClick={() => props.callback({data})}>
+              Add to cart
+            </button>
+          </div>
+        ))}
       </div>
     );
   }
 };
+
+const Cart = (props) => {
+  console.log(props.cartOpen);
+  if (props.cartOpen) {
+  return (
+    <div className="cart">
+      {props.items.map((data) => (
+        <p className="cartItem" key={"item" + data.ID}>
+         {data.Name}
+        </p>
+      ))}
+      <button className="myButton" type="submit">
+              Order
+      </button>
+    </div>
+  );
+  }
+}
+
+const ButtonCart = (props) => {
+  console.log(props.cart.length);
+  if (props.cart.length) {
+    return (
+      <button className="openCart" onClick={props.callback}>
+        Cart
+        <FaShoppingCart />
+      </button>
+    );
+  }
+
+}
 
 export default Catalog;
