1a5a9c26264b939ba86e3857b49c230ad3cf5d80
safym <osafym@gmail.com>
2022-08-16 00:12:20 +0300

    Изменила кнопку оформления заказа в зависисмости от авторизованности

--- src/components/cart/cartFooter.js
+++ src/components/cart/cartFooter.js
@@ -9,10 +9,28 @@ const CartFooter = (props) => {
     return (
       <div className="cartFooter">
         <h1 className="cartTotal">Total: {props.totalCoast} $</h1>
-        <button className="myButton">Checkout ></button>
+       
+        <BtnCheckout auth={props.auth}/>
       </div>
     );
   }
 };
+
+const BtnCheckout = (props) => {
+  console.log(props.auth)
+  let btnContent;
+  if (props.auth) {
+    btnContent = "Checkout >" 
+  } else {
+    btnContent = "Sign to checkout" 
+  }
+  // if (props.auth) {
+  //   return (<button className="myButton">Checkout ></button>)
+  // } else {
+  //   return (<button className="myButton" disabled="true">Sign to checkout</button>)
+  // }
+
+  return (<button className="myButton" disabled={!props.auth}>{btnContent}</button>)
+}
 
 export default CartFooter;
